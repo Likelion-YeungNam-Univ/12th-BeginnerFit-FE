fac260e6cd0d03d3b721111e3d3fc7bda06559ef
Yeongi Park <yeongi0111@naver.com>
2024-07-18 11:51:20 +0900

    main 페이지 생성후 라우터 등록 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/home/home";
+import { Main } from "./pages/main";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/main" element={<Main />}></Route>
       </Routes>
     </>
   );
