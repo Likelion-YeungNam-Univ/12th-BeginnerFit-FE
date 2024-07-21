6afb398142b819100f445c325b163368682ca24c
jihyun132 <jihyun.q.s@gmail.com>
2024-07-21 16:10:21 +0900

    feat: login 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/home/home";
+import Login from "./pages/login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login/>}></Route>
       </Routes>
     </>
   );
