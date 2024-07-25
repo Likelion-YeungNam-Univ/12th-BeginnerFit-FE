1286215a90ab2487ecb84fecb342cc14f7c17450
Yeongi Park <yeongi0111@naver.com>
2024-07-25 22:24:46 +0900

    feat : 친구 검색 기능 구현 완료

--- src/components/FriendList/UserList.jsx
+++ src/components/FriendList/UserList.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import user from "../../images/user.png";
 import { User } from "./User";
+import { useFriendSearchStore } from "../../store/useFriendSearchStore";
 
+// 서버로 부터 사용자 전체 받아오기
 const userList = [
   { id: 1, nickname: "zoedhj", image: user },
   { id: 2, nickname: "ywamcgj", image: user },
@@ -17,9 +19,31 @@ const userList = [
 ];
 
 export const UserList = () => {
+  // input값 가져오기
+  const { value } = useFriendSearchStore();
+
+  const [searchUser, setSearchUser] = useState(null);
+
+  // 무분별한 filter를 막기위해 0.3초 동안 입력이 없을 경우 해당 함수 수행
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // 아무것도 입력안 했을 때는 전체 사용자 가져오기
+      if (value == "") {
+        setSearchUser(userList);
+        return;
+      } else {
+        let arr = userList.filter((item) =>
+          item.nickname.toLowerCase().includes(value.toLowerCase())
+        );
+        setSearchUser(arr);
+      }
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [value]);
   return (
     <div>
-      {userList.map((item) => (
+      {searchUser?.map((item) => (
         <User
           key={item.id}
           id={item.id}
