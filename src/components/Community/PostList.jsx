0b54b2915c2b07fb14dcd97a385b7b99f8606207
tkv00 <tkv0098@gmail.com>
2024-07-26 00:53:39 +0900

    refactor:PostList useFetchData사용 로직변경 및 useFetchData훅에 배열형 데이터 추가

--- src/components/Community/PostList.jsx
+++ src/components/Community/PostList.jsx
@@ -8,50 +8,19 @@ import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import axios from "axios";
+import api from "../../apis/axios.jsx";
+import useFetchData from "../../hooks/useFetchData.jsx";
 
 export default function PostList() {
-  //const [posts, setPosts] = useState([]);
-  const TEST_SERVER_URL = import.meta.env.VITE_TEST_SERVER_URL;
-  const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-  const { id } = useParams();
-  const [loading, setLoading] = useState(true);
-  const [posts, setPosts] = useState([]);
 
-  //임시 토큰
-  const TOKEN = import.meta.env.VITE_TOKEN;
-
-  // axios 인스턴스 생성
-  const api = axios.create({
-    baseURL: "/api",
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-      "Content-Type": "application/json",
-    },
-  });
-
-  //게시글 불러오기함수
-  const getPost = async () => {
-    try {
-      const response = await api.get("/posts");
-      console.log("API Response:", response.data); // 응답 데이터 구조 확인
-      setPosts(response.data);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-  };
-  //apis 파일에 정리 필요
-
-  useEffect(() => {
-    getPost();
-  }, []);
+  const { arr, loading } = useFetchData("/posts");
 
   return (
     <>
       {loading ? (
         <h2>Loading...</h2>
       ) : (
-        posts.map((post) => (
+        arr.map((post) => (
           <Link
             key={post.id}
             to={`/posts/${post.id}`}
