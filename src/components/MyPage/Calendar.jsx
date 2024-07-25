5843064b902a091f66766a538c1353dff25043f1
Yeongi Park <yeongi0111@naver.com>
2024-07-25 16:15:01 +0900

    feat: 캘린더 ui 완료

---
+++ src/components/MyPage/Calendar.jsx
@@ -0,0 +1,49 @@
+import {
+  StyledCalendarWrapper,
+  StyledCalendar,
+  StyledToday,
+  StyledDot,
+} from "../../styles/CalendarStyle";
+import { useState } from "react";
+import dayjs from "dayjs";
+
+export default function Challender() {
+  const today = new Date();
+  const [date, setDate] = useState(today);
+
+  const attendDay = ["2024-07-03", "2024-07-02"]; // 출석한 날짜 예시
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+  };
+
+  return (
+    <StyledCalendarWrapper>
+      <StyledCalendar
+        value={date}
+        onChange={handleDateChange}
+        formatDay={(locale, date) => dayjs(date).format("D")} // 일 제거 숫자만 보이게
+        formatYear={(locale, date) => dayjs(date).format("YYYY")} // 네비게이션 눌렀을때 숫자 년도만 보이게
+        formatMonthYear={(locale, date) => dayjs(date).format("YYYY년 MM월")} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
+        calendarType="gregory" // 일요일 부터 시작
+        showNeighboringMonth={false} // 전달, 다음달 날짜 숨기기
+        next2Label={null} // +1년 & +10년 이동 버튼 숨기기
+        prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
+        minDetail="year" // 10년단위 년도 숨기기
+        // tileDisabled={() => true} // 모든 날짜 비활성화
+        tileContent={({ date, view }) => {
+          let html = [];
+          const dt = dayjs(date);
+          const todayDt = dayjs();
+
+          if (view === "month" && dt.isSame(todayDt, "day")) {
+            html.push(<StyledToday key={"today"}>오늘</StyledToday>);
+          }
+          if (attendDay.includes(dt.format("YYYY-MM-DD"))) {
+            html.push(<StyledDot key={dt.format("YYYY-MM-DD")} />);
+          }
+          return <>{html}</>;
+        }}
+      />
+    </StyledCalendarWrapper>
+  );
+}
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -8,6 +8,8 @@ import {
 import { Header } from "../layouts/header";
 import TagList from "../components/Main/TagList";
 import Weight from "../components/MyPage/Weight";
+import Calendar from "../components/MyPage/Calendar";
+import styled from "styled-components";
 
 export default function MyPage() {
   // 현재 달 구하는 코드
@@ -25,13 +27,25 @@ export default function MyPage() {
           </MainH2>
           <TagList />
           <Weight />
+          <P>내 정보 수정하기</P>
           <MainH3>
             사용자님의
             <br />
             {todayMonth}월 출석 캘린더
           </MainH3>
+          <Calendar />
         </MainContent>
       </MainContainer>
     </MainWrapper>
   );
 }
+
+const P = styled.p`
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.gray02};
+  margin: 10px 0px 0px 0px;
+  text-align: end;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
