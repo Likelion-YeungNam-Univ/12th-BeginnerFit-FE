36a2100e7b90cf7000a16b32da00e79c4ff31c9c
yeongipark <122340316+yeongipark@users.noreply.github.com>
2024-07-28 14:05:56 +0900

    fix : 배포를 위한 host와 post 설정

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
+      host: '0.0.0.0',
+      port: 5173,
       proxy: {
         "/api": {
           target: env.VITE_SERVER_URL,
