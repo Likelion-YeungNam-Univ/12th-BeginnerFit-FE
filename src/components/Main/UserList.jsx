ae1c81220a90543a455d8861126f70b8c0b0b1e0
Yeongi Park <yeongi0111@naver.com>
2024-07-31 14:38:45 +0900

    feat : 메인 화면 챌린지 순위 api 연동 완료

--- src/components/Main/UserList.jsx
+++ src/components/Main/UserList.jsx
@@ -1,23 +1,37 @@
 import styled from "styled-components";
 import User from "./User";
-import { bounce } from "./ChallengeList";
 import { SpeechBubble } from "./ChallengeList";
-
-const userList = [
-  { nickname: "연기" },
-  { nickname: "도연" },
-  { nickname: "지현" },
-  { nickname: "박하" },
-  { nickname: "서연" },
-  { nickname: "규희" },
-];
+import useFetchData from "../../hooks/useFetchData";
+import { useUserInfo } from "../../store/useUserInfo";
+import { useEffect, useState } from "react";
 
 export default function UserList() {
+  // 사용자 정보 저장할 배열 state
+  const [userList, setUserList] = useState([]);
+
+  // 나의 순위를 저장할 state
+  const [rank, setRank] = useState(0);
+
+  // 현재 로그인한 정보 가져오기
+  const { user } = useUserInfo();
+
+  // 사용자 랭킹 데이터 서버에서 받아오기
+  const { data } = useFetchData("/challengeparticipant/ranking");
+
+  useEffect(() => {
+    if (data) {
+      setUserList(data);
+      // 받아온 데이터 중 나의 순위 찾아서 저장하기
+      let myRank = data.find((item) => item.userId === user.userId).rank;
+      setRank(myRank);
+    }
+  }, [data]);
+
   return (
     <Container>
-      <SpeechBubble>사용자님은 현재 7위에요!</SpeechBubble>
-      {userList.map((item, idx) => (
-        <User {...item} rank={idx + 1} key={item.nickname} />
+      <SpeechBubble>사용자님은 현재 {rank}위에요!</SpeechBubble>
+      {userList?.map((item) => (
+        <User rank={item.rank} key={item.userId} nickname={item.name} />
       ))}
     </Container>
   );
